f2ad29148cfc46e7018ffdcbe7b6a99fe6398ca0
Darren <darren@darrenmothersele.com>
2016-05-22 12:58:49 +0100

    add toString for debugging vectors

--- src/Vec3.h
+++ src/Vec3.h
@@ -7,12 +7,18 @@
 
 #include <glm/vec3.hpp>
 #include <glm/geometric.hpp>
+#include <string>
 
 typedef glm::vec3 Vec3;
 
 Vec3 unitVector(Vec3 v)
 {
     return glm::normalize(v);
+}
+
+std::string toString(Vec3 v)
+{
+    return "[" + std::to_string(v.x) + "," + std::to_string(v.y) + "," + std::to_string(v.z) + "]";
 }
 
 #endif //RAY0_VEC3_H
